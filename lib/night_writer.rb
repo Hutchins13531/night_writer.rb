80f4c51a0a665050e0518683952d9e4e9e6e90e7
“Brad <hutch13531@gmail.com>
2022-04-25 17:08:37 -0400

    Feat: NightWriter - condition for confirmation_message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,4 +2,12 @@ require './lib/file_manager'
 
 class NightWriter
   argv = ARGV
+
+  if argv.length == 2
+    file = FileManager.new
+    file.read(argv[0])
+    file.write(argv[1])
+    outgoing_message_length = file.read_file.chars.count
+    puts confirmation_message
+  end
 end
--- module/messageable.rb
+++
@@ -1,9 +0,0 @@
-module Messagble
-
-  def confirmation_message(read_file)
-    puts "You've requested to read '#{read_file}'"
-    puts "and output the translated file to '#{output_file}'"
-    puts "Is that correct? Type 'y' for yes, 'n' for no. No quotes."
-  end
-
-end
