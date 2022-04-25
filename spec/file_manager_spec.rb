7ac69facfb3b457d1a2c3e534af77593e6ff7b08
“Brad <hutch13531@gmail.com>
2022-04-25 14:27:49 -0400

    Test: FileManager - has readable attributes

--- spec/file_manager_spec.rb
+++ spec/file_manager_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe FileManager do
   expect(file_manager).to be_an_instance_of(FileManager)
   end
 
+  it 'has readable attributes' do
+    file_manager = FileManager.new
+  expect(file_manager.read_file).to eq(nil)
+  end
+
+
+
+
 end
