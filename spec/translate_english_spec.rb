215053056aa471923684aebd8c31f77a72f16764
“Brad <hutch13531@gmail.com>
2022-04-26 15:03:01 -0400

    Test: TranslateEnglish - it has readable attributes

--- spec/translate_english_spec.rb
+++ spec/translate_english_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe TranslateEnglish do
     translator2 = TranslateEnglish.new(file_manager)
     expect(translator2).to be_an_instance_of(TranslateEnglish)
   end
+  it 'has readable attributes' do
+    file_manager = FileManager.new
+    translator2 = TranslateEnglish.new(file_manager)
+    dictionary = Dictionary.new
+  expect(translator2.file_manager).to eq(file_manager)
+  expect(translator2.translated_message).to eq([])
+  expect(translator2.dictionary).to be_an_instance_of(Dictionary)
+  end
 end
