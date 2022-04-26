976e17047d0fc29fc8c6bb15667ad8efab34afc8
“Brad <hutch13531@gmail.com>
2022-04-26 14:35:53 -0400

    Feat; NightReader/NightWriter - updated confirmation message

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,4 +1,17 @@
+require './lib/file_manager'
+require './module/Messageable'
+
 class NightReader
+  extend Messageable
 
-
+  argv = ARGV
+  if argv.length == 2
+    file = FileManager.new
+      file.read(argv[0])
+      file.write_braille(argv[1])
+      outgoing_message_length = File.read(argv[1]).chomp.length
+      confirmation_message2(argv[1], outgoing_message_length)
+  end
 end
+
+# ruby ./lib/night_reader.rb braille.txt original_message.txt
