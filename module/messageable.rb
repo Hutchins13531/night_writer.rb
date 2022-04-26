46d19f8553a0046acf76aa031672433453174d34
“Brad <hutch13531@gmail.com>
2022-04-26 14:41:35 -0400

    Feat: Module/Messageable - renamed methods to include confirmation_message2

--- module/messageable.rb
+++ module/messageable.rb
@@ -1,6 +1,10 @@
 module Messageable
 
-  def confirmation_message(outgoing_message, char_length)
+  def confirmation_message1(outgoing_message, char_length)
      puts "Created '#{outgoing_message}' containing #{char_length} characters."
+  end
+
+  def confirmation_message2(original_message, char_length)
+     puts "Created '#{original_message}' containing #{char_length} characters."
   end
 end
