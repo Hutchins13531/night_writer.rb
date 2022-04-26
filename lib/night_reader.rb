e4d02afe3f6ad397516bb4729277aeb6ab9563aa
“Brad <hutch13531@gmail.com>
2022-04-26 18:10:58 -0400

    Feat: Updated Runners

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -10,8 +10,7 @@ class NightReader
       file.read(argv[0])
       file.write_english(argv[1])
       outgoing_message_length = File.read(argv[1]).chomp.length
-      confirmation_message2(argv[1], outgoing_message_length)
+      Messageable.confirmation_message2(argv[1], outgoing_message_length)
   end
 end
-
 # ruby ./lib/night_reader.rb braille.txt original_message.txt
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -10,8 +10,7 @@ class NightWriter
       file.read(argv[0])
       file.write_braille(argv[1])
       outgoing_message_length = File.read(argv[1]).chomp.length
-      confirmation_message1(argv[1], outgoing_message_length)
+      Messageable.confirmation_message1(argv[1], outgoing_message_length)
   end
 end
-
 # ruby ./lib/night_writer.rb message.txt braille.txt
